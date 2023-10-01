70a226a9ac24ef86d11745a5eae4fd03b8e9cda9
Joren Thijs <joren.thijs@hotmail.com>
2023-10-01 17:08:46 +0200

    added music and buttons to main menu

---
+++ src/main_menu/main_menu.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class main_menu : Node2D
+{
+    public override void _Ready()
+    {
+
+    }
+
+    private void _on_new_game_pressed()
+    {
+        GD.Print("Here");
+    }
+
+    private void _on_load_game_pressed()
+    {
+        GD.Print("Here");
+    }
+
+    private void _on_controls_pressed()
+    {
+        GD.Print("Here");
+    }
+}
