94c9c563579e7de92446712654f55c60ade0b936
Joren Thijs <joren.thijs@hotmail.com>
2025-03-23 15:12:59 +0100

    fix: move common code to character node class

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,6 +1,67 @@
 class_name Character
-
 extends CharacterBody2D
+
+const MOVEMENT_SPEED := 2000
+var direction: Vector2 = Vector2.ZERO
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var navigation_target: Node2D = null
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var navigation_path_refresh_timer: Timer = $NavigationAgent2D/NavigationPathRefresh
+
+func _ready() -> void:
+	navigation_agent.navigation_finished.connect(_on_navigation_agent_navigation_finished)
+	navigation_path_refresh_timer.timeout.connect(_on_navigation_path_refresh_timeout)
+	if navigation_target != null:
+		navigation_agent.target_position = navigation_target.global_position
+		
+func set_navigation_target(node: Node2D) -> void:
+	navigation_target = node
+		
+func _physics_process(delta: float) -> void:
+	get_movement()
+	velocity = direction * MOVEMENT_SPEED * delta
+	move_and_slide()
+
+func get_movement() -> void:
+	if navigation_target == null:
+		direction = Vector2.ZERO
+		animated_sprite.stop()
+		return
+	if !navigation_agent.is_target_reached():
+		direction = to_local(navigation_agent.get_next_path_position()).normalized()
+		
+		var horizontal = abs(direction.x) > abs(direction.y)
+		
+		if !horizontal and direction.y < 0:
+			animated_sprite.play("walk_up")
+		elif !horizontal and direction.y > 0:
+			animated_sprite.play("walk_down")
+		elif horizontal and direction.x < 0:
+			animated_sprite.play("walk_left")
+		elif horizontal and direction.x > 0:
+			animated_sprite.play("walk_right")
+		else:
+			animated_sprite.stop()
+		
+		move_and_slide()
+	else:
+		direction = Vector2.ZERO
+		animated_sprite.stop()
+	if !navigation_agent.is_target_reachable():
+		direction = Vector2.ZERO
+		animated_sprite.stop()
+
+func _on_navigation_agent_navigation_finished() -> void:
+	pass
+
+func _on_navigation_path_refresh_timeout() -> void:
+	if navigation_target == null:
+		return
+	if navigation_agent.target_position != navigation_target.global_position:
+		navigation_agent.target_position = navigation_target.global_position
+	navigation_path_refresh_timer.start()
 
 func interact() -> void:
 	talk()
--- scripts/forest_scene.gd
+++ scripts/forest_scene.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+@onready var jeffrey: Jeffrey = %Jeffrey
+@onready var walk_away_target: Node2D = %"Walk away target"
+
 func _ready() -> void:
 	MusicManager.play_music.emit(Music.Songs.FOREST_SCENE)
+	
+func walk_away_jeffrey() -> void:
+	jeffrey.set_navigation_target(walk_away_target)
+
+func _on_jeffrey_player_invited() -> void:
+	print("player invited")
+	walk_away_jeffrey()
