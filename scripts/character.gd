42eca39479a6013b749898ec3b0a801dcf0d4a6f
Joren Thijs <joren.thijs@hotmail.com>
2025-03-23 15:33:12 +0100

    fix: organized character code

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,31 +1,37 @@
 class_name Character
 extends CharacterBody2D
 
-const MOVEMENT_SPEED := 2000
+@export var movement_speed: int = 3000
 var direction: Vector2 = Vector2.ZERO
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var navigation_target: Node2D = null
-@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
-@onready var navigation_path_refresh_timer: Timer = $NavigationAgent2D/NavigationPathRefresh
-
 func _ready() -> void:
-	navigation_agent.navigation_finished.connect(_on_navigation_agent_navigation_finished)
-	navigation_path_refresh_timer.timeout.connect(_on_navigation_path_refresh_timeout)
-	if navigation_target != null:
-		navigation_agent.target_position = navigation_target.global_position
-		
-func set_navigation_target(node: Node2D) -> void:
-	navigation_target = node
+	setup_navigation()
 		
 func _physics_process(delta: float) -> void:
 	get_movement()
-	velocity = direction * MOVEMENT_SPEED * delta
+	velocity = direction * movement_speed * delta
 	move_and_slide()
+	
+#region navigation
+@export var navigation_target: Node2D = null
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+var is_navigating: bool:
+	get: return navigation_target != null
+@onready var navigation_path_refresh_timer: Timer = $NavigationAgent2D/NavigationPathRefresh
+
+func setup_navigation() -> void:
+	navigation_agent.navigation_finished.connect(_on_navigation_agent_navigation_finished)
+	navigation_path_refresh_timer.timeout.connect(_on_navigation_path_refresh_timeout)
+	if is_navigating:
+		navigation_agent.target_position = navigation_target.global_position
+
+func set_navigation_target(node: Node2D) -> void:
+	navigation_target = node
 
 func get_movement() -> void:
-	if navigation_target == null:
+	if !is_navigating:
 		direction = Vector2.ZERO
 		animated_sprite.stop()
 		return
@@ -44,8 +50,6 @@ func get_movement() -> void:
 			animated_sprite.play("walk_right")
 		else:
 			animated_sprite.stop()
-		
-		move_and_slide()
 	else:
 		direction = Vector2.ZERO
 		animated_sprite.stop()
@@ -57,11 +61,12 @@ func _on_navigation_agent_navigation_finished() -> void:
 	pass
 
 func _on_navigation_path_refresh_timeout() -> void:
-	if navigation_target == null:
+	if !is_navigating:
 		return
 	if navigation_agent.target_position != navigation_target.global_position:
 		navigation_agent.target_position = navigation_target.global_position
 	navigation_path_refresh_timer.start()
+#endregion
 
 func interact() -> void:
 	talk()
