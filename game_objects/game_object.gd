b5b9c51fe4f6187514a64b966848de121d8c00ba
Joren Thijs <joren.thijs@hotmail.com>
2025-04-01 21:33:20 +0200

    feat: added notification system

--- game_objects/game_object.gd
+++ game_objects/game_object.gd
@@ -1,14 +1,16 @@
 class_name GameObject extends StaticBody2D
 
+@export var dialogue: DialogueResource
 @export var item: InventoryItem
 @export var note: Note
-@export var dialogue: DialogueResource
 
 func interact():
+    if dialogue != null:
+        DialogueManager.show_dialogue_balloon(dialogue)
+        await DialogueManager.dialogue_ended
     if item != null:
         InventoryManager.add_inventory_item(item)
     elif note != null:
         InventoryManager.add_note(note)
-    elif dialogue != null:
-        DialogueManager.show_dialogue_balloon(dialogue)
+    
     
---
+++ globals/notification_manager.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+
+@onready var notification_label: Label = $NotificationControl/MarginContainer/Panel/NotificationLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var notification_timer: Timer = Timer.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    hide()
+    InventoryManager.inventory_item_added.connect(display_inventory_notification)
+    InventoryManager.note_added.connect(display_note_notification)
+    setup_timer()
+
+func setup_timer():
+    add_child(notification_timer)
+    notification_timer.one_shot = true
+    notification_timer.wait_time = 3.0
+    notification_timer.timeout.connect(on_notification_timer_timeout)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
+
+func display_inventory_notification(item: InventoryItem) -> void:
+    notification_label.text = "Item %s was added to your inventory" % item.name
+    animation_player.play("show_notification")
+    await animation_player.animation_finished
+    notification_timer.start()
+    
+func display_note_notification(note: Note) -> void:
+    notification_label.text = "You made a note about %s" % note.name
+    animation_player.play("show_notification")
+    await animation_player.animation_finished
+    notification_timer.start()
+    
+func on_notification_timer_timeout() -> void:
+    animation_player.play("hide_notification")
